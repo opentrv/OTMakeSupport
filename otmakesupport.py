69f0583e5c85df0424d24e3302842c78de5b0104
Deniz Erbilgin <denzo@denzo-desktop>
2016-05-13 17:45:51 +0100

    Can download and update repos with python

--- otmakesupport.py
+++ otmakesupport.py
@@ -5,6 +5,7 @@ import os
 
 join = os.path.join
 
+# Path names
 REPO_REMOTES = {'opentrv': 'https://github.com/DamonHD/OpenTRV',
                 'otradiolink' : 'https://github.com/opentrv/OTRadioLink',
                 'otaesgcm' : 'https://github.com/opentrv/OTAESGCM'}
@@ -13,13 +14,12 @@ REPO_PATH = '/home/denzo/OpenTRV/OTMakeSupport_test'
 REPO_LOCALS = {'opentrv': 'opentrv', 'otradiolink': 'otradiolink', 'otaesgcm': 'otaesgcm'}
 ARDUINO_BIN = '/home/denzo/OpenTRV/Arduino'
 
-#repo = Repo.init(my_repo, bare=True)
+my_repos = None
+
+# TODO this is the wrong way round.
 try:
-    my_repos = {'opentrv': Repo.clone_from(REPO_REMOTES['opentrv'], join(REPO_PATH, REPO_LOCALS['opentrv']), branch=REPO_BRANCH),
-                'otradiolink': Repo.clone_from(REPO_REMOTES['otradiolink'], join(REPO_PATH, REPO_LOCALS['otradiolink']), branch=REPO_BRANCH),
-                'otaesgcm': Repo.clone_from(REPO_REMOTES['otaesgcm'], join(REPO_PATH, REPO_LOCALS['otaesgcm']), branch=REPO_BRANCH)}
-except:
-    print("Folder not empty. Try adding local repos.")
+    #
+    print("Updating repos")
     my_repos = {'opentrv': Repo(join(REPO_PATH, REPO_LOCALS['opentrv'])),
                 'otradiolink': Repo(join(REPO_PATH, REPO_LOCALS['otradiolink'])),
                 'otaesgcm': Repo(join(REPO_PATH, REPO_LOCALS['otaesgcm']))}
@@ -29,5 +29,14 @@ except:
         repo.heads.master.checkout()
         repo.remotes.origin.pull()
     {k: update_repo(repo) for k, repo in my_repos.items()}
+except:
+    # Attempt to clone REPO_REMOTES
+    print("Local repos not found. Clone to " + REPO_PATH + "?")
+    response = input("[Y/n]")
+    print("downloading...")
+    if (response == 'Y') or (response == 'y') or (response == '\n'):
+        my_repos = {'opentrv': Repo.clone_from(REPO_REMOTES['opentrv'], join(REPO_PATH, REPO_LOCALS['opentrv']), branch=REPO_BRANCH),
+                    'otradiolink': Repo.clone_from(REPO_REMOTES['otradiolink'], join(REPO_PATH, REPO_LOCALS['otradiolink']), branch=REPO_BRANCH),
+                    'otaesgcm': Repo.clone_from(REPO_REMOTES['otaesgcm'], join(REPO_PATH, REPO_LOCALS['otaesgcm']), branch=REPO_BRANCH)}
 
 print("success")
