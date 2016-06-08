52d60dd79b6d3dd325d51862ece68f67dd08203f
Deniz Erbilgin <denzo1@gmail.com>
2016-06-08 15:44:15 +0100

    moved file paths to variables

--- Programming Scripts/fuse-bootl.sh
+++ Programming Scripts/fuse-bootl.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-sudo /home/mark/m/dev/arduino/arduino-1.6.5-r5/hardware/tools/avr/bin/avrdude -C/home/mark/m/dev/arduino/arduino-1.6.5-r5/hardware/tools/avr/etc/avrdude.conf  -patmega328p -c usbtiny -e -Ulock:w:0x3F:m -Uefuse:w:0x06:m -Uhfuse:w:0xde:m -Ulfuse:w:0x42:m 
+TOOLS=~/openTRV/arduino-1.6.8
+BOOTLOADER=~/.arduino15/packages/opentrv/hardware/avr/0.0.4/bootloaders/atmega328_opentrv_v0p2.hex
+
+sudo $TOOLS/hardware/tools/avr/bin/avrdude -C$TOOLS/hardware/tools/avr/etc/avrdude.conf  -patmega328p -c usbtiny -e -Ulock:w:0x3F:m -Uefuse:w:0x06:m -Uhfuse:w:0xde:m -Ulfuse:w:0x42:m 
 
 #sudo /usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf
-sudo /home/mark/m/dev/arduino/arduino-1.6.5-r5/hardware/tools/avr/bin/avrdude -C/home/mark/m/dev/arduino/arduino-1.6.5-r5/hardware/tools/avr/etc/avrdude.conf  -patmega328p -c usbtiny -Uflash:w:/home/mark/m/dev/arduino/arduino-1.6.5-r5/hardware/arduino/avr/bootloaders/atmega/atmega328_1b.hex:i -Ulock:w:0x2F:m 
+sudo $TOOLS/hardware/tools/avr/bin/avrdude -C$TOOLS/hardware/tools/avr/etc/avrdude.conf  -patmega328p -c usbtiny -Uflash:w:$BOOTLOADER:i -Ulock:w:0x2F:m 
 
 ###sudo /usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf  -patmega328p -cusbtiny -Uflash:w:V0p2_Main.cpp.hex:i
 
