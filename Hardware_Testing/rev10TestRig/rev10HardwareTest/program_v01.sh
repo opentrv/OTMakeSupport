17af5e79e4212887705ffcf91952f8f25bfcb5de
Deniz Erbilgin <denzo@denzo-desktop>
2016-07-27 11:10:44 +0100

    added version numbers

---
+++ Hardware_Testing/rev10TestRig/rev10HardwareTest/program_v01.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Script for programming OpenTRV V0p2 boards using a USBTiny ISP.
+# 
+# Notes:
+# - Change ARDUINO to point to your arduino installation.
+# - Change HEXFILE to the name of the file you wish to burn.
+# - Run the script from the directory containing the hex file.
+
+HEXFILE=REV10_Hardware_Tests.ino.hex
+
+sudo avrdude -patmega328p -cusbtiny -Uflash:w:$HEXFILE:i
+
