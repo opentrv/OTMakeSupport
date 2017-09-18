92fe90fdc92bffa0a39e440bdd77b459dd7ec8b0
Denzo77 <Denzo77@users.noreply.github.com>
2017-09-18 13:28:34 +0100

    Update fuse-bootl.sh

--- Programming Scripts/fuse-bootl.sh
+++ Programming Scripts/fuse-bootl.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# *************************************************************
+#
+# The OpenTRV project licenses this file to you
+# under the Apache Licence, Version 2.0 (the "Licence");
+# you may not use this file except in compliance
+# with the Licence. You may obtain a copy of the Licence at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Licence is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the Licence for the
+# specific language governing permissions and limitations
+# under the Licence.
+#
+# *************************************************************
+# Author(s) / Copyright (s): Deniz Erbilgin 2016
+#                            Mark Hill 2016
+
 TOOLS=~/openTRV/arduino-1.6.8
 BOOTLOADER=~/.arduino15/packages/opentrv/hardware/avr/0.0.4/bootloaders/atmega328_opentrv_v0p2.hex
 
